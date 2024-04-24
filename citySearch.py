f4e4c05860e868494bfabc29c5a813b6637709d6
Aditya Parihar <adityaparihar5050@gmail.com>
2024-04-24 16:11:35 +0530

    first commit

---
+++ citySearch.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    city = request.args.get('city')
+    api_key = ''
+    response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}')
+    return response.json()
+
+if __name__ == '__main__':
+    app.run(port=5000)
